2c4265ae4fdecf1b2c84be0b904830612f8d3ab4
yahooachun <73005852+yahooachun@users.noreply.github.com>
2021-05-10 00:25:46 +0800

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,56 @@
+new fullpage('#fullpage', {
+	//options here
+	autoScrolling:true,
+	scrollHorizontally: true,
+    onLeave: function(origin, destination, direction){
+        var loading2 = new TimelineMax();
+        loading2.fromTo(".upper",0.6,{
+            autoAlpha: 0,
+
+        },{
+            autoAlpha: 1,
+
+        },"+=0.5")
+        .fromTo(".lower",0.6,{
+            autoAlpha: 0,
+
+        },{
+            autoAlpha: 1,
+
+        },"-=0.6")
+
+        ;
+    },
+});
+
+document.addEventListener("DOMContentLoaded",function(){
+    var loading = new TimelineMax();
+    loading.fromTo(".upper h2",0.6,{
+        autoAlpha: 0,
+        y: 20
+    },{
+        autoAlpha: 1,
+        y: 0
+    })
+    .fromTo(".upper p",0.6,{
+        autoAlpha: 0,
+
+    },{
+        autoAlpha: 1,
+
+    })
+    .fromTo(".btn1",0.6,{
+        autoAlpha: 0,
+        x: -20
+    },{
+        autoAlpha: 1,
+        x: 0
+    })
+    .fromTo(".arrow",0.6,{
+        autoAlpha: 0,
+        y: -20
+    },{
+        autoAlpha: 1,
+        y: 0
+    })
+});
